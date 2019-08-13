97a81b2d6cc1fc1a00a2bc7fd481e48f1c973e98
Emil Małańczak <kdddmalanczak@gmail.com>
2019-08-13 18:45:43 +0200

    something

--- src/index.js
+++ src/index.js
@@ -1,7 +1,47 @@
-import './css/style.css';
-import Board from './js/Board';
+class Board {
+    constructor() {
+        this.body = this.create();
+    }
 
+    create() {
+        const boardArray = [];
+        for (let i = 0; i < 10; i++) {
+            boardArray.push([]);
+            for (let j = 0; j < 10; j++) {
+                boardArray[i].push("");
+            }
+        }
+        return boardArray;
+    }
+}
 
+class Player {
+    constructor() {
+    this.myArmy= new Board()
+    this.myFights= new Board()
+    }
+    generateBoards(){
+        const boards = [
+            [this.myArmy, '#battlefield', 'field']
+            [this.myFights, '#battlefield-prev', 'field-prev']
+        ];
+        boards.forEach(e => {
+            for (let i = 0 ; i < 10 ; i++) {
+                for (let j = 0; j < 10 ; j++) {
+                    const field = document.createElement('div');
+                    field.innerHTML = e[0][i][j];
+                    field.className = e[2];
+                    document.querySelector(e[1]).appendChild(field);
+                }
+            }
+        });
+    }
+}
+
+const player1 = new Player();
+const player2 = new Player();
+
+player1.generateBoards()
 //for testing purpose - to remove!!!
 const tab = new Board();
 console.log(tab);
--- src/js/Player.js
+++ src/js/Player.js
@@ -1,7 +1,28 @@
-const board = require(./Board);
+
 class Player {
-    constuctor() {
-    this.myArmy=new Board();
-    this.myFights=new Board();
+    constructor() {
+    this.myArmy= new Board()
+    this.myFights= new Board()
+    }
+    generateBoards(){
+        const boards = [
+            [this.myArmy, '#battlefield', 'field']
+            [this.myFights, '#battlefield-prev', 'field-prev']
+        ];
+        boards.forEach(e => {
+            for (let i = 0 ; i < 10 ; i++) {
+                for (let j = 0; j < 10 ; j++) {
+                    const field = document.createElement('div');
+                    field.innerHTML = e[0][i][j];
+                    field.className = e[2];
+                    document.querySelector(e[1]).appendChild(field);
+                }
+            }
+        });
     }
 }
+
+const player1 = new Player();
+const player2 = new Player();
+
+player1.generateBoards()
