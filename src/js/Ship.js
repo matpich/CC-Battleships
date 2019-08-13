228970c0be129bd51998660ef20dea341ff4c428
wallgravve <46935150+wallgravve@users.noreply.github.com>
2019-08-13 19:14:33 +0200

    Add files via upload
    Added classes for ships

---
+++ src/js/Ship.js
@@ -0,0 +1,45 @@
+class Ship {
+	constructor(x,y) {
+		this.type = 'none';
+		this.size = 0;
+	}
+	
+	ToString() {
+		return `SHIP: ${this.type} ${this.size}`;
+	}
+}
+
+class Carrier extends Ship {
+	constructor(x,y) {
+		super(x,y);
+		this.type = 'carrier';
+		this.size = 5;
+	}	
+}
+
+
+class Battleship extends Ship {
+	constructor(x,y) {
+		super(x,y);
+		this.type = 'battleship';
+		this.size = 4;
+	}	
+}
+
+class Destroyer extends Ship {
+	constructor(x,y) {
+		super(x,y);
+		this.type = 'destroyer';
+		this.size = 3;
+	}	
+}
+
+class Submarine extends Ship {
+	constructor(x,y) {
+		super(x,y);
+		this.type = 'submarine';
+		this.size = 2;
+	}	
+}
+
+export { Ship,Battleship,Destroyer };
